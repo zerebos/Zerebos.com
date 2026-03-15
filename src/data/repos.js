b153f573a9aecd5f4888490b09224c5a71736f95
Zerebos <me@zerebos.com>
2026-03-14 21:22:21 -0400

    Add remaining repos

--- src/data/repos.js
+++ src/data/repos.js
@@ -1,31 +1,55 @@
-export default [
-    "BetterDiscordAddons",
-    "EnhancedDiscordPlugins",
-    "BDPluginLibrary",
-    "Nox",
-    "discordbot.py",
-    "BlockCatcher",
-    "BlockCatcherFX",
-    "BBDInstaller",
-    "Hangman",
-    "TableTennisDB",
-    "Planum",
-    "BoiBot",
-    "Object3D-Demo",
-    "VHDL-Communications",
-    "Intelligent-Line-And-Marker-Tracking-Car",
-    "BetterDiscord/Installer",
+// TODO: dynamically all repos from GitHub API instead of hardcoding them here
+// Should use a high cache time since repos don't change that often, and this will reduce the number of API calls significantly
+export const betterdiscord = [
     "BetterDiscord/BetterDiscord",
+    "BetterDiscord/Installer",
     "BetterDiscord/docs",
-    "Trilium-Breadcrumbs",
+    "BetterDiscord/cli",
+    "BetterDiscord/PublicServers",
+    "BetterDiscord/BetterDiscordBot",
+];
+
+export const personal = [
+    "ghostty-config",
+    "BetterDiscordAddons",
+    "BDPluginLibrary",
+    "BBDInstaller",
+    "Nox",
     "Trilium-SingleFile",
+    "Trilium-Breadcrumbs",
     "Trilium-MarkdownPreview",
+    "trilium.rocks",
+    "discordbot.py",
     "Trilium-LaTeXPreview",
+    "VHDL-Communications",
+    "trilium-etapi",
     "trilium-types",
     "trilium-pack",
-    "trilium-etapi",
-    "ghostty-config",
-    "trilium.rocks",
+    "BlockCatcher",
+    "EnhancedDiscordPlugins",
+    "TableTennisDB",
+    "Planum",
     "dotfiles",
-    "asar.net"
+    "better-formatting",
+    "BoiBot",
+    "ModernRenai",
+    "Citador",
+    "asar.net",
+    "Intelligent-Line-And-Marker-Tracking-Car",
+    "BlockCatcherFX",
+    "BetterDiscordPlugins",
+    "EnhancedDiscord",
+    "Object3D-Demo",
+    "Hangman",
+    "Tools-and-Utilities",
 ];
+
+
+const jssucks = [
+    "JsSucks/BetterDiscordApp",
+    "JsSucks/BDI",
+    "JsSucks/BDEdit",
+];
+
+
+export default [...betterdiscord, ...personal, ...jssucks];
