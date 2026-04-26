208ae3a606ac5ed245741eb585b189affd1b737c
Zerebos <me@zerebos.com>
2026-04-26 19:34:23 -0400

    Fix cloudflare build hopefully

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import {defineConfig} from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
-    site: "https://zerebos.com",
-    output: "static",
-    integrations: [sitemap()],
-    markdown: {
-        smartypants: false
-    },
-    build: {
-        format: "directory"
-    }
+  site: "https://zerebos.com",
+  output: "static",
+  integrations: [sitemap()],
+
+  markdown: {
+      smartypants: false
+  },
+
+  build: {
+      format: "directory"
+  },
+
+  adapter: cloudflare()
 });
