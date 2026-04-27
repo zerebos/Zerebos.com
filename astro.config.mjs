87cab1487b6765489547ec2c6e90a34c75771770
Zerebos <me@zerebos.com>
2026-04-26 21:25:23 -0400

    Remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 import {defineConfig} from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
-  site: "https://zerebos.com",
-  output: "static",
-  integrations: [sitemap()],
+    site: "https://zerebos.com",
+    output: "static",
+    integrations: [sitemap()],
 
-  markdown: {
-      smartypants: false
-  },
+    markdown: {
+        smartypants: false
+    },
 
-  build: {
-      format: "directory"
-  },
+    build: {
+        format: "directory"
+    },
 
-  adapter: cloudflare()
+    experimental: {
+        rustCompiler: true
+    }
 });
